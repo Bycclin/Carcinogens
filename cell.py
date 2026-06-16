c73a70a4e7a671c75038ee6a79031c3696c64ef7
enzowaterdingus-cmyk <enzowaterdingus@gmail.com>
2026-06-16 13:09:26 -0700

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,6 +1,6 @@
 import pygame
 from dynamicPoint import *
-from constants import *
+import constants as c
 class cell:
     def __init__(self, x, y, radius):
         self.x = x
@@ -10,9 +10,9 @@ class cell:
         
     
     def draw(self, screen):
-        pygame.draw.circle(screen, "blue", (self.point.x, self.point.y), self.radius*ZOOM, 2)
-        pygame.draw.line(screen, "blue", (self.point.x+0.7011*self.radius*ZOOM, self.point.y+0.7011*self.radius*ZOOM), (self.point.x-0.7011*self.radius*ZOOM, self.point.y-0.7011*self.radius*ZOOM), 2)
-        pygame.draw.line(screen, "blue", (self.point.x-0.7011*self.radius*ZOOM, self.point.y+0.7011*self.radius*ZOOM), (self.point.x+0.7011*self.radius*ZOOM, self.point.y-0.7011*self.radius*ZOOM), 2)
+        pygame.draw.circle(screen, "blue", (self.point.x, self.point.y), self.radius*c.ZOOM, 2)
+        pygame.draw.line(screen, "blue", (self.point.x+0.7011*self.radius*c.ZOOM, self.point.y+0.7011*self.radius*c.ZOOM), (self.point.x-0.7011*self.radius*c.ZOOM, self.point.y-0.7011*self.radius*c.ZOOM), 2)
+        pygame.draw.line(screen, "blue", (self.point.x-0.7011*self.radius*c.ZOOM, self.point.y+0.7011*self.radius*c.ZOOM), (self.point.x+0.7011*self.radius*c.ZOOM, self.point.y-0.7011*self.radius*c.ZOOM), 2)
 
     def update(self):
         self.point.x = self.x
@@ -22,5 +22,6 @@ class cell:
         
        
         #offset based off scroll
-        self.point = self.point.translate(-SCROLL_X, -SCROLL_Y)
+        self.point = self.point.translate(-c.SCROLL_X, -c.SCROLL_Y)
+        
         
