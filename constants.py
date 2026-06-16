2197e00b46743ce046804ed2c1393f8751d8c2e1
enzowaterdingus-cmyk <enzowaterdingus@gmail.com>
2026-06-16 12:55:23 -0700

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+import pygame
+WIDTH = 800
+HEIGHT = 800
+CENTER_X = WIDTH/2
+CENTER_Y = HEIGHT/2
+
+#game engine components
+SCROLL_X = 0
+SCROLL_Y = 0
+DIRECTION = 0 # in degrees
+ZOOM = 1 #in times scale
+
+
