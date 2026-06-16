80777c7ff9e85deb8d7b1dd9b602400288e546f7
enzowaterdingus-cmyk <enzowaterdingus@gmail.com>
2026-06-16 14:47:57 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,6 +1,5 @@
 import pygame
 from dynamicPoint import *
-#from constants import *
 import constants
 class Player:
     def __init__(self, x, y):
@@ -18,6 +17,7 @@ class Player:
         pygame.draw.rect(screen, "red", self.dev_shape, 2)
         pygame.draw.line(screen, "red", (self.dev_shape.left, self.dev_shape.top), (self.dev_shape.right, self.dev_shape.bottom), 2)
         pygame.draw.line(screen, "red", (self.dev_shape.right, self.dev_shape.top), (self.dev_shape.left, self.dev_shape.bottom), 2)
+
 
     def update_controls(self):
         
@@ -45,6 +45,9 @@ class Player:
         
         self.vx *= 0.9
         self.vy *= 0.9
+
+        constants.SCROLL_X += ((self.x-constants.CENTER_X) - constants.SCROLL_X)/4
+        constants.SCROLL_Y += ((self.y-constants.CENTER_Y) - constants.SCROLL_Y)/4
 
         constants.SCROLL_X += ((self.x-constants.CENTER_X) - constants.SCROLL_X)/4
         constants.SCROLL_Y += ((self.y-constants.CENTER_Y) - constants.SCROLL_Y)/4
