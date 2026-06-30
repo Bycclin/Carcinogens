e72bfecb09deb72fdd457dbd870ef8dc0256df0a
Bycclin <186978689+Bycclin@users.noreply.github.com>
2026-06-30 11:40:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def gradual_text(text: str, x, y,):
         text_surface = constants.GAME_FONT.render(text[:i], True, "black")
         screen.blit(text_surface, (x, y))
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(16000)
         pygame.time.wait(50)
 
 
@@ -369,6 +369,10 @@ while running:
 
 
 OPENING_CUTSCENE.close()
+
+pygame.mixer.init()
+background_music = pygame.mixer.music.load("5462458621362176.wav")
+pygame.mixer.music.play(loops=-1, fade_ms=2000)
 
 #do slide sequence
 
